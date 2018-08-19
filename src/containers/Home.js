327195d69d4af7878003d329879b53d238bbf251
Agustin Villalobos <agustinvillalobos@protonmail.com>
2018-08-19 08:12:51 -0600

    Adding graphql

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,39 @@
 import React from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
 
-class Home extends React.Component {
-    render() {
-        return(
-            <div>Home</div>
-        )
-    }
+const Home = () => {
+    return(
+        <div className='container'>
+            <Query query={FETCH_POSTS}>
+                {({ loading, error, data }) => {
+                    console.log(data)
+                    if (loading) return "Loading...";
+                    if (error) return `Error! ${error.message}`
+
+                    return (
+                        <div>
+                            {data.allPosts.map(post => (
+                                <div key={post.id}>
+                                    {post.post}
+                                </div>
+                            ))}
+                        </div> 
+                    );
+                }}
+            </Query>
+        </div>
+    )
 }
+
+const FETCH_POSTS = gql`
+    {
+        allPosts {
+            post,
+            id,
+            createdAt
+        }
+    }
+`;
 
 export default Home
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
+import ApolloClient from "apollo-boost"
+import { ApolloProvider } from "react-apollo"
+
 
 import './styles/index.css'
 import App from './containers/App'
 import registerServiceWorker from './config/registerServiceWorker'
 
+
+const client = new ApolloClient({
+    uri: 'https://api.graph.cool/simple/v1/cjl0i4k9450n80155aqv83dw1'
+  });
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ApolloProvider>
     , document.getElementById('root'));
 registerServiceWorker();
