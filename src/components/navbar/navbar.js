9766fb59b03071adacf65f924b9aa85d36f42e2a
Agustin Villalobos <agustinvillalobos@protonmail.com>
2018-08-19 10:33:35 -0600

    Adding assets

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Book from '../../assets/img/book.svg'
+
+const navbar = () => {
+    return(
+        <nav className="navbar navbar-light bg-light">
+            <div className='container'>
+                <a className="navbar-brand">
+                    <img src={Book} alt="" />
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export default navbar
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Routes from './Routes'
+import NavBar from '../components/navbar/navbar'
 
 
 class App extends React.Component {
     render() {
         return (
-            <Routes />
+            <React.Fragment>
+                <NavBar />
+                <Routes />
+            </React.Fragment>  
         );
     }
 }
