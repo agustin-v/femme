f8591ee4faf71fcfa8be829c26822b041e0f16a0
Agustin Villalobos <agustinvillalobos@protonmail.com>
2018-08-23 17:22:45 -0600

    - Removing console.log - Adding Link to each post

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
+import {Link} from 'react-router-dom'
 
 import PostPlaceholder from '../components/placeholder/PostPlaceholder'
 import PostPreview from '../components/PostPreview/PostPreview'
@@ -10,13 +11,12 @@ const Home = () => {
         <div className='container'>
             <Query query={FETCH_POSTS}>
                 {({ loading, error, data }) => {
-                    console.log(data)
                     if (loading) return (<PostPlaceholder/>)
                     if (error) return `Error! ${error.message}`
                     return (
                         <div>
                             {data.allPosts.map(post => (
-                                < PostPreview key={post.id} post={post.post} title={post.title}/>
+                                <Link to={`/post/${post.id}`} key={post.id} style={{textDecoration: 'none'}}>< PostPreview post={post.post} title={post.title}/></Link>
                             ))}
                         </div> 
                     )
